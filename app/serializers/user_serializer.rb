9e165f1fb5d124a7e4281be74aa67eb4c425979b
johnnie71 <johnnie.agonz71@gmail.com>
2020-12-10 14:24:06 -0500

    added dependent destroy for Designs for a User and cleaned up some code

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,19 +3,15 @@ class UserSerializer < ActiveModel::Serializer
     has_many :designs
     has_many :comments
 
-     # we need to serialze to get the propic as url and to remove sensitve/extra data from array of users
-  def isFollowing
-    
-    object.isFollowing.map { |user| { id: user.id, name: user.name, username: user.username, profile_pic: user.profile_pic, bio: user.bio } }
-  
-  end
+     
+    def isFollowing
+      object.isFollowing.map { |user| { id: user.id, name: user.name, username: user.username, profile_pic: user.profile_pic, bio: user.bio } }
+    end
 
-  # we need to serialze to get the propic as url and to remove sensitve/extra data from array of users
-  def isFollowedBy
-    
-    object.isFollowedBy.map { |user| { id: user.id, name: user.name, username: user.username, profile_pic: user.profile_pic, bio: user.bio } }
-    
-  end
+  
+    def isFollowedBy
+      object.isFollowedBy.map { |user| { id: user.id, name: user.name, username: user.username, profile_pic: user.profile_pic, bio: user.bio } }
+    end
 
   end
   
